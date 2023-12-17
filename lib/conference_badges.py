16f16fd2d65727282e00de08b0f2e8aef739e683
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-12-17 12:50:20 +0300

    Assignment Completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = range(1,9)
+    speaker_rooms = []
+    
+    for i , name in enumerate(names):
+        if i < len(rooms):
+            room_number = rooms[i]
+            speaker_rooms.append(f"Hello, {name}! You'll be assigned to room {room_number}!")
+        else:
+            break
+    return speaker_rooms
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for assignment in rooms:
+        print(assignment)
